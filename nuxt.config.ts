cb6026bbed95f355c3fb4f0f417a10f5ba2e89da
Leon Vogler <leonfvogler@gmail.com>
2026-02-03 00:35:51 +0100

    chore: update devtools configuration in nuxt.config.ts; switch to using import.meta.dev for improved environment detection

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: process.env.NODE_ENV === 'development' },
+  devtools: { enabled: import.meta.dev },
   css: ['~/assets/css/main.css'],
   modules: [
     '@nuxt/content',
